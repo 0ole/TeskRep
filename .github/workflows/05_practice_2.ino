364e2105f508dacfef608b78a1174988ce43967e
0ole <117723295+0ole@users.noreply.github.com>
2025-09-15 10:07:52 +0900

    Create 05_practice_2.ino

---
+++ .github/workflows/05_practice_2.ino
@@ -0,0 +1,27 @@
+#define LED_PIN 7  // GPIO 7번 핀에 LED 연결
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(LED_PIN, LOW);   
+    delay(100);                   
+    digitalWrite(LED_PIN, HIGH);  
+    delay(100);                   
+  }
+
+  
+  digitalWrite(LED_PIN, HIGH);
+
+
+  while (1) {
+  }
+}
+
